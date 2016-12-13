41b302cfa2d10d5b2350596788fc8dc944922fa3
Juraci Vieira <juraci.vieira@gmail.com>
2016-12-13 10:03:33 -0200

    moving songs to songs route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,31 +1,4 @@
 import Ember from 'ember';
 
-let Song = Ember.Object.extend({
-  title: '',
-  band: '',
-  rating: 0
-});
-
 export default Ember.Route.extend({
-  model() {
-    let blackDog = Song.create({
-      title: 'Black Dog',
-      band: 'Led Zeppelin',
-      rating: 3
-    });
-
-    let yellowLedbetter = Song.create({
-      title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
-      rating: 4
-    });
-
-    let pretender = Song.create({
-      title: 'The Pretender',
-      band: 'Foo Fighters',
-      rating: 2
-    });
-
-    return [blackDog, yellowLedbetter, pretender];
-  }
 });
